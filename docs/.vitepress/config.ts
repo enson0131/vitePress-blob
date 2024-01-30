cbd6d2e73a7b9d4fca45bfca1f1582112fa63d34
enson_chen <1004714132@qq.com>
2024-01-30 14:13:59 +0800

    fix: 🐞 删除网络内容导航栏

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -136,7 +136,7 @@ function sidebarGuide(): DefaultTheme.SidebarItem[] {
           { text: 'DNS', link: '网络相关/DNS' },
           { text: 'CDN', link: '网络相关/CDN' },
           { text: '跨域请求', link: '网络相关/跨域请求'},
-          { text: '常见的问题', link: '网络相关/常见的问题'}
+          // { text: '常见的问题', link: '网络相关/常见的问题'}
         ]
       },
       {
